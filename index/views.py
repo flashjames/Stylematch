94f0d9d5e079563175a7b41b01803d6ca65b3dfa
Jenso <jenso1988@gmail.com>
2012-02-25 17:19:12 +0100

    initial commit.

---
+++ index/views.py
@@ -0,0 +1,17 @@
+# Create your views here.
+from django.http import HttpResponse
+from annoying.decorators import render_to
+from django.shortcuts import render_to_response
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+
+@render_to('index.html')
+def index(request):
+    return {}
+
+def logout_page(request):
+    """
+    Log users out and re-direct them to the main page.
+    """
+    auth.logout(request)
+    return HttpResponseRedirect('/')

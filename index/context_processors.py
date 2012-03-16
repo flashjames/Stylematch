b3c5d85910edf9093c3fb9c71020561c7bd39b9e
Jenso <jenso1988@gmail.com>
2012-03-16 20:51:57 +0100

    removed dependence on django-galleria

--- index/context_processors.py
+++ index/context_processors.py
@@ -2,3 +2,19 @@ from django.conf import settings # import the settings file
 
 def jquery_script(context):
     return {'JQUERY_SCRIPT': settings.JQUERY_SCRIPT}
+
+# links to galleria jquery library
+def galleria_urls(request):
+    static_url = getattr(settings, 'STATIC_URL')
+    galleria_url = getattr(
+        settings, 'GALLERIA_URL', '%sgalleria/src/' % static_url)
+    galleria_script = getattr(
+        settings, 'GALLERIA_SCRIPT', '%sgalleria.js' % galleria_url)
+    galleria_theme = getattr(
+        settings, 'GALLERIA_THEME',
+        '%sthemes/classic/galleria.classic.js' % galleria_url)
+    return {
+        'GALLERIA_URL': galleria_url,
+        'GALLERIA_SCRIPT': galleria_script,
+        'GALLERIA_THEME': galleria_theme,
+    }

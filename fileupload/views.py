ca0b34758ce34d6537e58f56b31af07f513c9c3b
Jenso <jenso1988@gmail.com>
2012-03-17 10:52:48 +0100

    fileupload view now sends validation errors back in json, this is mostly a hack

--- fileupload/views.py
+++ fileupload/views.py
@@ -4,7 +4,7 @@ from django.views.generic import CreateView, DeleteView
 from django.http import HttpResponse
 from django.utils import simplejson
 from django.core.urlresolvers import reverse
-from django.core.exceptions import ValidationError
+from django.forms.util import ErrorList
 
 from django.conf import settings
 
@@ -27,25 +27,40 @@ class PictureCreateView(CreateView):
     # Add some custom validation to our image field.
     # TODO: Should override a function, earlier in validation flow
     # than form_valid() where it's called right now
-    def clean_image(self,image):
+    def clean_image(self,image,form):
         if image:
             if image._size > settings.MAX_IMAGE_SIZE:
-                raise ValidationError("Image file too large ( > 20mb )")
-            return image
+                form._errors['file'] = 'Image is too large'
+                return False
+                #raise ValidationError("Image file too large ( > 20mb )")
+            return True
         else:
-            raise ValidationError("Couldn't read uploaded image")
+            form._errors['file'] = [u'Couldnt read uploaded image']
+            return False
+            #raise ValidationError("Couldn't read uploaded image")
+
+        
 
     def get_form(self, form_class):
         form = super(PictureCreateView, self).get_form(form_class)
         form.instance.user = self.request.user
-        #pdb.set_trace()
         return form
 
     # TODO: should return error in JSONformat, so upload interface
     # can display the error message, and appropriate action for user
     # errors is contained in form._errors
     def form_invalid(self, form):
-        return self.render_to_response(self.get_context_data(form=form))
+        error_msg = ""
+        for error in form._errors.values():
+            if isinstance(error, str):
+                error_msg += error
+            else:
+                error_msg += error.as_text()
+ 
+        data = [{'error': error_msg}]
+        response = JSONResponse(data, {}, response_mimetype(self.request))
+        response['Content-Disposition'] = 'inline; filename=files.json'
+        return response
     
     # Called when we're sure all fields in the form are valid
     def form_valid(self, form):
@@ -53,28 +68,25 @@ class PictureCreateView(CreateView):
         f = self.request.FILES.get('file')
 
         # check if image size is ok
-        self.clean_image(f)
-
-        filename=get_unique_filename(f.name)
-        self.object = form.save(commit=False)
-        self.object.save(user=self.request.user,filename=filename)
+        if(not self.clean_image(f,form)):
+            return HttpResponse(self.form_invalid(form))
+        else:
+            filename=get_unique_filename(f.name)
+            self.object = form.save(commit=False)
+            self.object.save(user=self.request.user,filename=filename)
         
-        image_url = self.object.get_image_url()
+            image_url = self.object.get_image_url()
 
-        data = [{'name': f.name, 'url': image_url, 'thumbnail_url': image_url, 'delete_url': reverse('upload-delete', args=[self.object.id]), 'delete_type': "DELETE"}]
+            data = [{'name': f.name, 'url': image_url, 'thumbnail_url': image_url, 'delete_url': reverse('upload-delete', args=[self.object.id]), 'delete_type': "DELETE"}]
 
-        response = JSONResponse(data, {}, response_mimetype(self.request))
-        response['Content-Disposition'] = 'inline; filename=files.json'
-        return response
+            response = JSONResponse(data, {}, response_mimetype(self.request))
+            response['Content-Disposition'] = 'inline; filename=files.json'
+            return response
 
 class PictureDeleteView(DeleteView):
     model = Picture
 
     def delete(self, request, *args, **kwargs):
-        """
-        This does not actually delete the file, only the database record.
-        Implement w
-        """
         self.object = self.get_object()
         self.object.delete()
         response = JSONResponse(True, {}, response_mimetype(self.request))

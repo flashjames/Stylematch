d1ca7ed45e81dff70ede67ed642a732aa61e6101
Jenso <jenso1988@gmail.com>
2012-03-27 10:45:04 +0200

    Added new class-based view to display formset for Service's. Also changed the class-based view so we can access user object in the formset -> display only the user's services.

--- accounts/views.py
+++ accounts/views.py
@@ -28,70 +28,56 @@ class ServiceForm(ModelForm):
 from enhanced_cbv.views.edit import EnhancedFormSet, EnhancedModelFormSet
 
 class ServiceEnhancedModelFormSet(EnhancedModelFormSet):
-    queryset = Service.objects.filter(user__exact=1)
     
-    def __init__(self, asd, *args, **kwargs):
-        print asd
-        myvalues = kwargs.pop('hej', "bleh")
-        #print it to console (this is a dict by default)
-        print myvalues
-        #self.request = kwargs.pop('request', None)        
-        #queryset = Service.objects.filter(user__exact=1)#self.request.user.id)
-        super(ServiceEnhancedModelFormSet, self).__init__(*args, **kwargs)
+    
+    def get_base_formset(self, user, **kwargs):
+        """
+        Returns the base formset
+        """
+        # CHANGED: set the queryset to filter services on the user that requested the view 
+        self.queryset = Service.objects.filter(user__exact=user.id)
         
+        new_kwargs = self.get_kwargs()
+        new_kwargs.update(**kwargs)
+        return self.get_factory()(**new_kwargs)
+
     form_class = ServiceForm
     model = Service
-    
+
     extra = 1
 
-#user_id = None
 
 class ServicesModelView(ModelFormSetsView):
-
-    def get_initial(self):
-        """ Passing initial values to the form"""
-        # At first initial data should be saved
-        super(ServicesModelView, self).get_initial()
-        # adding  our values
-        print "haj:" + self.request.user
-        self.initial= {"myuser": self.request.user}
-        return self.initial
-
-
-    def instantiate_enhanced_formsets(self):
-        self.enhanced_formsets_instances = []
-        for formset in self.formsets:
-            #import pdb;pdb.set_trace()
-            print self.request
-            enhanced_formset_instance = formset("hi2")
-            self.enhanced_formsets_instances.append(enhanced_formset_instance)
-        
-    def get_formsets_kwargs(self, enhanced_formset):
-        print "hi!"
-        kwargs = super(ServicesModelView, self).get_formsets_kwargs(
-                                                                 enhanced_formset)
-        kwargs.update({
-            'queryset': enhanced_formset.get_queryset(),
-            #'hej': 'test',
-            })
-
-        #global user_id = self.request.user.id
-        #kwargs.update({
-        #    
-        #    })
-
-        return kwargs
-
-
-    #def get(self, request, *args, **kwargs):
-    #    #print request
-    #    super(ServicesModelView, self).__init__(*args, **kwargs)
-    #    return self.render_to_response(self.get_context_data())
-                
     formsets = [ServiceEnhancedModelFormSet]
-        
+
     template_name = 'authors_articles.html'
     success_url = '/success/'
+    
+    def construct_formsets(self):
+        """
+        Constructs the formsets
+        """
+        self.formsets_instances = []
+        print self.request
+
+        prefixes = {}
+        for enhanced_formset in self.enhanced_formsets_instances:
+
+            # CHANGED: added parameter request.user
+            base_formset = enhanced_formset.get_base_formset(self.request.user,
+                **self.get_factory_kwargs())
+
+            prefix = base_formset.get_default_prefix()
+            prefixes[prefix] = prefixes.get(prefix, 0) + 1
+            if prefixes[prefix] != 1:
+                prefix = "%s-%s" % (prefix, prefixes[prefix])
+
+            self.formsets_instances.append(
+                base_formset(prefix=prefix, **self.get_formsets_kwargs(
+                    enhanced_formset))
+            )
+
+   
 
 
 ServiceFormSet = modelformset_factory(Service,form=ServiceForm)
@@ -115,11 +101,11 @@ class ServiceEditView(CreateView, LoginRequiredMixin):
         #context = self.get_context_data()
         formset = ServiceFormSet(request.POST)
         #formset = context['formset']
- 	if formset.is_valid():
+        if formset.is_valid():
             import pdb
             pdb.set_trace()
             pass
-            # do something with the cleaned_data on the formsets. 
+            # do something with the cleaned_data on the formsets.
 
     #def get_form(self, form_class):
     #    form = super(ServiceEditView,self).get_form(form_class) #instantiate using parent
@@ -132,7 +118,7 @@ class ServiceEditView(CreateView, LoginRequiredMixin):
 
     def get_context_data(self, **kwargs):
         context = super(ServiceEditView, self).get_context_data(**kwargs)
-        
+
         if self.request.POST:
             context['formset'] = ServiceFormSet(self.request.POST)
         else:
@@ -141,16 +127,16 @@ class ServiceEditView(CreateView, LoginRequiredMixin):
 
 class ServiceListView(LoginRequiredMixin, ListView):
     context_object_name = "services"
-    
+
     def get_queryset(self):
         return Service.objects.filter(user__exact=self.request.user.id)
 
 class ServiceCreateView(LoginRequiredMixin, CreateView):
     model = Service
-    
+
     def __init__(self, *args, **kwargs):
         super(ServiceCreateView, self).__init__(*args, **kwargs)
-        
+
         # written here in init since it will give reverse url error
         # if just written in class definition. because urls.py isnt loaded
         # when this class is defined

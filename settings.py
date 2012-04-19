6e30a82a93b971bcfff7da1bd28c5036186f1e79
Jenso <jenso1988@gmail.com>
2012-04-19 16:51:22 +0200

    fix

--- settings.py
+++ settings.py
@@ -96,6 +96,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    PROJECT_DIR+"/media/",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.

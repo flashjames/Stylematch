a0f3db741fbeb001a3ceac88ac4f50ed485a58aa
Jenso <jenso1988@gmail.com>
2012-06-01 16:24:37 +0200

    Refactored views in index.view
    Removed views that could use a TemplateView directly in urls.py.

--- index/views.py
+++ index/views.py
@@ -3,23 +3,17 @@
 # from django.shortcuts import render_to_response
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import ListView, CreateView
 from accounts.models import UserProfile, Picture
 from index.models import BetaEmail
 
 from django.core.urlresolvers import reverse
-from django.shortcuts import redirect
 
-class AboutPageView(TemplateView):
-    """
-    Display about us page
-    """
-    template_name = "about_us.html"
 
 class InspirationPageView(ListView):
     """
     """
-    context_object_name = "pictures"    #default is object_list
+    context_object_name = "pictures"
     template_name = "index.html"
     queryset = Picture.objects.order_by('-upload_date')
     paginate_by = 30
@@ -30,11 +24,6 @@ class InspirationPageView(ListView):
     
     template_name = "inspiration.html"
 
-class ContactPageView(TemplateView):
-    """
-    Display contact us page
-    """
-    template_name = "contact_us.html"
     
 class BetaEmailView(CreateView):
     """
@@ -51,25 +40,7 @@ class BetaEmailView(CreateView):
         # when this class is defined
         
         self.success_url=reverse('beta_index_page')
-    
-class FeaturesPageView(TemplateView):
-    """
-    Display features page
-    """
-    template_name = "features.html"
-    
-class SignupStep1PageView(TemplateView):
-    """
-    Display signup step 1 page
-    """
-    template_name = "signup_step1.html"
 
-class SignupStep2PageView(TemplateView):
-    """
-    Display signup step 2 page
-    """
-    template_name = "signup_step2.html"
-    
 
 class IndexPageView(ListView):
     """
@@ -83,7 +54,6 @@ class IndexPageView(ListView):
         context = super(IndexPageView, self).get_context_data(**kwargs)
         return context
         
-
 
 def logout_page(request):
     """

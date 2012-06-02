47a12282f5bb3456d49f9e7d119a8884a1d527f4
Samuel Andersson <smulster@gmail.com>
2012-06-02 17:26:00 +0200

    Refactored urls and fabfile to pep8 style.

--- fabfile.py
+++ fabfile.py
@@ -13,6 +13,7 @@ TODO: Write local deploy script with local()
 instead of current bash script.
 """
 
+
 def virtualenv(command):
     """
     Execute commands in virtualenv, as env.deploy_user
@@ -20,24 +21,32 @@ def virtualenv(command):
     with cd(env.directory):
         sudo(env.activate + '&&' + command, user=env.deploy_user)
 
+
 def git_pull():
     'Updates the repository.'
     with cd(env.directory):
         sudo('git pull', user=env.deploy_user)
 
+
 def update_dependencies():
     virtualenv("pip install -r requirements.txt")
-    
+
+
 def upload_static_files():
     virtualenv("./manage.py collectstatic --noinput")
 
+
 def compile_less():
-    virtualenv("lessc bootstrap/static/less/bootstrap.less bootstrap/static/css/style.css")
+    virtualenv("lessc bootstrap/static/less/bootstrap.less "
+                     "bootstrap/static/css/style.css")
+
 
 def backup_database():
     now = datetime.datetime.now()
-    filename = "/home/ubuntu/database_backup/stylematchdb_" + now.strftime("%y%m%d-%H-%M")
+    filename = ("/home/ubuntu/database_backup/stylematchdb_"
+                + now.strftime("%y%m%d-%H-%M"))
     sudo("mysqldump -u root -p django_stylematch > " + filename)
+
 
 def migrate_db():
     backup_database()
@@ -49,19 +58,24 @@ def migrate_db():
     virtualenv("./manage.py schemamigration accounts --auto")
     virtualenv("./manage.py migrate")
 
+
 def restart_nginx():
     sudo("/etc/init.d/nginx restart")
 
+
 def top():
     sudo("top")
+
 
 def update_git_submodules():
     with cd(env.directory):
         sudo('git submodule init', user=env.deploy_user)
         sudo('git submodule update', user=env.deploy_user)
-    
+
+
 def restart_gunicorn():
     sudo("restart django_stylematch")
+
 
 def deploy_db_change():
     #if confirm("Continue?"):
@@ -70,6 +84,7 @@ def deploy_db_change():
     compile_less()
     upload_static_files()
     restart_gunicorn()
+
 
 def deploy_without_db_change():
     update_dependencies()
--- urls.py
+++ urls.py
@@ -6,49 +6,115 @@ from index.views import IndexPageView, BetaEmailView, InspirationPageView
 from django.contrib import admin
 from django.views.generic.simple import redirect_to
 from registration.views import register
-from accounts.register_views import UserRegistrationForm, SignupView, SignupStep2View, SignupStep3View, SignupStep4View
-
+from accounts.register_views import (UserRegistrationForm,
+                                     SignupView,
+                                     SignupStep2View,
+                                     SignupStep3View,
+                                     SignupStep4View)
 admin.autodiscover()
 
 """
 WHEN ADDING NEW URLS
 https://docs.djangoproject.com/en/dev/ref/settings/#append-slash
 
-Django have a setting called APPEND_SLASH, if Django doesnt find for example /index
-it will redirect to /index/ and try that instead (if APPEND_SLASH=True which it is by default).
+Django have a setting called APPEND_SLASH, if Django doesnt find for example
+/index it will redirect to /index/ and try that instead
+(if APPEND_SLASH=True which it is by default).
 -> Always set urls with slash at the end.
 """
 
 
 urlpatterns = patterns(
     '',
-    (r'^logout/$', 'index.views.logout_page',{},'logout'), # redirects to main-page
-    url(r'', include('social_auth.urls')), # urls to connect with social media accountsm for example facebook
-     url(r'^accounts/register/$', register, {'backend': 'accounts.register_views.RegisterCustomBackend','form_class': UserRegistrationForm}, name='registration_register'),
-    url(r'accounts/register/complete/', redirect_to, {'url': '/accounts/signup-step1'}, 'registration_complete'),
-    (r'', include('registration.auth_urls')), # login/logout/password-management urls             
-    url(r'^admin/', include(admin.site.urls)),
-    (r'^$', IndexPageView.as_view(),{}, 'index_page'),
-        
-    (r'^accounts/signup-step1/', SignupView.as_view(),{}, 'signupstep1_page'),
-    (r'^accounts/signup-step2/', SignupStep2View.as_view(),{}, 'signupstep2_page'),
-    (r'^accounts/signup-step3/', SignupStep3View.as_view(),{}, 'signupstep3_page'),
-    (r'^accounts/signup-step4/', SignupStep4View.as_view(),{}, 'signupstep4_page'),
+    # redirects to main-page
+    (r'^logout/$',
+            'index.views.logout_page',
+            {},
+            'logout'),
+    # urls to connect with social media accountsm for example facebook
+    url(r'',
+            include('social_auth.urls')),
+    url(r'^accounts/register/$',
+            register,
+            {'backend': 'accounts.register_views.RegisterCustomBackend',
+             'form_class': UserRegistrationForm},
+            name='registration_register'),
+    url(r'accounts/register/complete/',
+            redirect_to,
+            {'url': '/accounts/signup-step1'},
+            'registration_complete'),
+    # login/logout/password-management urls
+    (r'',
+            include('registration.auth_urls')),
+    url(r'^admin/',
+            include(admin.site.urls)),
+    (r'^$',
+            IndexPageView.as_view(),
+            {},
+            'index_page'),
 
-    (r'^accounts/edit-settings/', TemplateView.as_view(template_name="edit-account-settings.html"),{},'edit-account-settings'),
-    (r'^about-us/', TemplateView.as_view(template_name="about_us.html"), {}, 'about_page'),
-    (r'^kontakt/', TemplateView.as_view(template_name="contact_us.html"),{}, 'contact_page'),
-    
-    (r'^get_invite', BetaEmailView.as_view(),{}, 'get_invite'),
-    (r'^start', TemplateView.as_view(template_name="betaemail_index.html"), {}, 'beta_index_page'),
-    
-    (r'^features/', TemplateView.as_view(template_name="features.html"),{}, 'features_page'),
-    (r'^anvandarvillkor/', TemplateView.as_view(template_name="anvandarvillkor.html"),{}, 'anvandarvillkor'),
-    url(r"^su/", include("django_su.urls")),
-   
-    (r'^google66ca7050dfade3e4.html', TemplateView.as_view(template_name="google66ca7050dfade3e4.html")), #tracking-code so google apps know we own the domain.
-   (r'^inspiration', InspirationPageView.as_view(),{},'inspiration-page'),
-    url(r'^', include('accounts.urls')), # accounts/profile, should always be at the end. since a user may set a profile url that match another url -> if it's not at the end it may overwrite it.
+    (r'^accounts/signup-step1/',
+            SignupView.as_view(),
+            {},
+            'signupstep1_page'),
+    (r'^accounts/signup-step2/',
+            SignupStep2View.as_view(),
+            {},
+            'signupstep2_page'),
+    (r'^accounts/signup-step3/',
+            SignupStep3View.as_view(),
+            {},
+            'signupstep3_page'),
+    (r'^accounts/signup-step4/',
+            SignupStep4View.as_view(),
+            {},
+            'signupstep4_page'),
+
+    (r'^accounts/edit-settings/',
+            TemplateView.as_view(template_name="edit-account-settings.html"),
+            {},
+            'edit-account-settings'),
+    (r'^about-us/',
+            TemplateView.as_view(template_name="about_us.html"),
+            {},
+            'about_page'),
+    (r'^kontakt/',
+            TemplateView.as_view(template_name="contact_us.html"),
+            {},
+            'contact_page'),
+
+    (r'^get_invite',
+            BetaEmailView.as_view(),
+            {},
+            'get_invite'),
+    (r'^start',
+            TemplateView.as_view(template_name="betaemail_index.html"),
+            {},
+            'beta_index_page'),
+
+    (r'^features/',
+            TemplateView.as_view(template_name="features.html"),
+            {},
+            'features_page'),
+    (r'^anvandarvillkor/',
+            TemplateView.as_view(template_name="anvandarvillkor.html"),
+            {},
+            'anvandarvillkor'),
+    url(r"^su/",
+            include("django_su.urls")),
+
+    # tracking-code so google apps know we own the domain.
+    (r'^google66ca7050dfade3e4.html',
+            TemplateView.as_view(template_name="google66ca7050dfade3e4.html")),
+    (r'^inspiration',
+            InspirationPageView.as_view(),
+            {},
+            'inspiration-page'),
+    # accounts/profile, should always be at the end. since a user may set a
+    # profile url that match another url -> if it's not at the end it may
+    # overwrite it.
+    url(r'^',
+            include('accounts.urls')),
 )
 
 if settings.DEBUG:

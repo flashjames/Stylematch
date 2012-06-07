23c40bfbdb520e4a3da8b4ccefbc3d8ce9ce69d4
Samuel Andersson <smulster@gmail.com>
2012-06-07 13:13:33 +0200

    Added test for registering a new user. WIP
    The test recieves error code 505 (HTTP version not supported) when
    button.click() is called. Trying to figure out why.

--- fts/tests.py
+++ fts/tests.py
@@ -4,6 +4,65 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 
+class RegisterTest(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.browser = WebDriver()
+        super(RegisterTest, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(RegisterTest, cls).tearDownClass()
+        cls.browser.quit()
+
+
+    def test_successful_register(self):
+        # open browser and navigate to stylematch
+        self.browser.get(self.live_server_url)
+
+        # make sure it loaded
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn(u"Hitta en kvalificerad frisör "
+                      u"i ditt närområde och boka nu", body.text)
+
+        # go to register page
+        register = self.browser.find_element_by_link_text(
+                                        u'Bli medlem som frisör')
+        register.click()
+        WebDriverWait(self.browser, 10).until(
+                lambda driver: driver.find_element_by_tag_name('body'))
+
+        # fill in the form
+        first_name = self.browser.find_element_by_name('first_name')
+        last_name = self.browser.find_element_by_name('last_name')
+        username = self.browser.find_element_by_name('username')
+        password1 = self.browser.find_element_by_name('password1')
+        password2 = self.browser.find_element_by_name('password2')
+        invite_code = self.browser.find_element_by_name('invite_code')
+        first_name.send_keys('testuser')
+        last_name.send_keys('usertester')
+        username.send_keys('test@example.com')
+        password1.send_keys('asdf1234')
+        password2.send_keys('asdf1234')
+        invite_code.send_keys('permanent1')
+
+        # click the "Skapa konto" button
+        button = self.browser.find_element_by_tag_name('button')
+        button.click()
+        WebDriverWait(self.browser, 10).until(
+                lambda driver: driver.find_element_by_tag_name('body'))
+
+        # make sure we logged in
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn(u'Hej testuser', body.text)
+
+        # make sure we arrived at signup-step1
+        self.assertIn(u"Först, börja med att fylla i "
+                      u"information om din salong", body.text)
+
+
+
 class AboutStyleMatchTest(LiveServerTestCase):
 
     @classmethod
@@ -32,3 +91,5 @@ class AboutStyleMatchTest(LiveServerTestCase):
         # find the text "Vilka är StyleMatch?"
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn(u"Vilka är StyleMatch?", body.text)
+
+

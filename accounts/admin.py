e3567c298d738e5bfd8fa228389ed0d256703ea7
Samuel Andersson <smulster@gmail.com>
2012-06-14 15:41:54 +0200

    Added link to django user in UserProfile admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,19 @@
 from accounts.models import UserProfile, GalleryImage, ProfileImage, InviteCode
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
 
 class UserProfileAdmin(admin.ModelAdmin):
     readonly_fields = ('temporary_profile_url',
                        'profile_image_cropped',
-                       'profile_image_uncropped',)
+                       'profile_image_uncropped',
+                       'user_link',)
+
+    def user_link(self, obj):
+        change_url = reverse('admin:auth_user_change', args=(obj.user.id,))
+        return mark_safe('<a href="%s">%s</a>' % (change_url, obj.user.email))
+
+    user_link.short_description = 'User'
 
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(ProfileImage)

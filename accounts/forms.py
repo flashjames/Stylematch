2d75a55608f500a9cd08dfda3903335239540431
Jenso <jenso1988@gmail.com>
2012-07-11 20:03:38 +0200

    Added .jpeg to allowed image formats

--- accounts/forms.py
+++ accounts/forms.py
@@ -126,7 +126,7 @@ class GalleryImageForm(forms.ModelForm):
                 raise ValidationError("Bilden är för stor ( > %s )"
                                     % convert_bytes(settings.MAX_IMAGE_SIZE))
             filename = file._name.lower() 
-            if not filename.endswith(('.jpg', '.gif', '.png')):
+            if not filename.endswith(('.jpg', '.gif', '.png', '.jpeg')):
                 raise ValidationError("Endast bildfiler i formaten "
                                       "PNG, JPG och GIF är accepterade.")
 

167dd177558b5e4a9f7c205d6915f1bab6811b88
Samuel Andersson <smulster@gmail.com>
2012-07-17 15:25:32 +0200

    Added dynamic google maps for search page
     - Works like regular city_search
     - Zoom is connected to the boundaries of all markers. Maximum zoom
       level is 12.

--- staticfiles/js/search.js
+++ staticfiles/js/search.js
@@ -5,40 +5,92 @@
         cache: false
     });
 
-    window.Map = Backbone.Model.extend({});
-
-    window.MapView = Backbone.View.extend({
-        tagName: 'div',
-        className:'map',
-        latitude:'59.33',
-        longitude:'18.07',
-
-        initialize: function() {
-            this.latlng = new google.maps.LatLng(this.latitude, this.longitude);
-
-            var myOptions = {
-              center: this.latlng,
-              zoom: 14,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-
-            this.map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-
-            _.bindAll(this, 'render');
-            this.render();
-        },
-        render: function() {
-            return this;
-        }
-    });
-
-
     window.Profile = Backbone.Model.extend({});
 
     window.ProfileCollection = Backbone.Collection.extend({
         model:Profile,
         // Set by the django template
         url: PROFILE_API_URL,
+    });
+
+    window.Map = Backbone.Model.extend({});
+
+    window.MapView = Backbone.View.extend({
+        el: $('#map_canvas'),
+        model: Map,
+        tagName: 'div',
+        className:'map',
+        latitude:'59.33',
+        longitude:'18.07',
+        maxZoom: 12, // maximum zoom achieved when refreshing the list
+
+        initialize: function(opts) {
+
+            this.profileList = opts.ProfileList;
+
+            var myOptions = {
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            this.map = new google.maps.Map(this.el, myOptions);
+            this.markerBounds = new google.maps.LatLngBounds();
+
+            // add listener for the bounds. If the bounds are too small the zoom could be too high.
+            // This prevents the zoom from going above this.maxZoom
+            var self = this; // ``this`` will start pointing on the map
+            google.maps.event.addListener(this.map, 'zoom_changed', function() {
+                zoomChangeBoundsListener =
+                    google.maps.event.addListener(self.map, 'bounds_changed', function(event) {
+                        if (this.getZoom() > self.maxZoom && this.initialZoom == true) {
+                            this.setZoom(self.maxZoom);
+                            this.initialZoom = false;
+                        }
+                    google.maps.event.removeListener(zoomChangeBoundsListener);
+                });
+            });
+            this.map.initialZoom = true;
+            this.map.fitBounds(this.markerBounds);
+
+            _.bindAll(this, 'render');
+            this.render();
+        },
+        render: function() {
+            // for some reason it didn't work passing ``this`` as a context
+            // variable to the underscore method
+            var self = this;
+
+            var geocoder = new google.maps.Geocoder();
+
+            _.each(this.profileList.models, function(profile) {
+
+                // create an address string
+                var address = profile.attributes.salon_adress + ' ' +
+                              profile.attributes.zip_adress + ', ' +
+                              profile.attributes.salon_city;
+
+                // convert an address to a LatLng position and put a marker there
+                geocoder.geocode({ 'address': address }, function(result, status) {
+                    if (status === 'OK') {
+                        // create a location
+                        var loc = new google.maps.LatLng(result[0].geometry.location.lat(),
+                                                         result[0].geometry.location.lng());
+
+                        // extend the markerBounds
+                        self.markerBounds.extend(loc);
+
+                        // make sure the map fits all markers
+                        self.map.fitBounds(self.markerBounds);
+
+                        // Add the marker
+                        var marker = new google.maps.Marker({
+                            position: loc,
+                            map: self.map,
+                            title: profile.attributes.salon_name
+                        });
+                    }
+                });
+            });
+            return this;
+        }
     });
 
     window.ProfileListView = Backbone.View.extend({
@@ -58,7 +110,7 @@
     window.ProfileListItemView = Backbone.View.extend({
         tagName:"li",
         className: "profile-list-item-city",
-        template:_.template($('#tpl-profile-list-item').html()),
+        template: _.template($('#tpl-profile-list-item').html()),
 
         render:function (eventName) {
             $(this.el).html(this.template(this.model.toJSON()));
@@ -68,9 +120,8 @@
 
     window.ProfileView = Backbone.View.extend({
         el: $("body"),
-        initialize:function () {
-
-            // Load profile with default values
+        initialize:function (opts) {
+            this.profileList = new ProfileCollection();
             this.filter();
         },
         events:{
@@ -78,7 +129,6 @@
             "change #city":"filter"*/
         },
         filter: function() {
-            this.profileList = new ProfileCollection();
 
             data = {};
             city = $('#city').val();
@@ -98,6 +148,7 @@
                             type: 'error'
                         });
                     }
+                    var mapView = new MapView({ ProfileList: collection });
                     $('#profile-list').html(new ProfileListView({model:collection}).render().el);
                 }
             });
@@ -105,7 +156,6 @@
     });
 
     this.ProfileView = new ProfileView();
-    this.MapView = new MapView();
 
 
 })(jQuery);
